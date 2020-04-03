754783bbc6d7e0d796e4b30b1fbcd2766a55d2f7
KipVader <61040190+KipVader@users.noreply.github.com>
2020-04-03 20:44:23 +0300

    init

---
+++ precache-manifest.65fecc7a335b54eb8df1d9fc49bef805.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5087299e0360ddb85fd044292aec0332",
+    "url": "/index.html"
+  },
+  {
+    "revision": "24c6072abb5a03a145b2",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "b4a2c7be4b5f805cede3",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "24c6072abb5a03a145b2",
+    "url": "/static/js/2.b03643ac.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.b03643ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4a2c7be4b5f805cede3",
+    "url": "/static/js/main.ff8c5863.chunk.js"
+  },
+  {
+    "revision": "5c6d8f0df7de8f9aaece",
+    "url": "/static/js/runtime-main.1cb24129.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
